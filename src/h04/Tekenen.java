f464976b058779966cff5c1c6fa87452309a7027
timijzebaart <tijzebaart@roc-dev.com>
2019-10-03 13:55:49 +0200

    Tekenen

---
+++ src/h04/Tekenen.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Tekenen extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.drawRect(20, 20, 100, 50);
+        g.fillRect(20, 80, 100, 50);
+    }
+}
