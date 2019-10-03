0a4227eceb30201a7ca252f3035d94434ae38134
timijzebaart <tijzebaart@roc-dev.com>
2019-10-03 09:34:37 +0200

    Java Applet

---
+++ src/h01/Show.java
@@ -0,0 +1,33 @@
+
+    /*
+
+@(#)Show.java 1.0 03/02/26
+
+*
+
+Voorbeeld 2.1
+
+*
+
+*/
+
+//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+import java.applet.*;
+
+    //een klasse met de naam Show van het type Applet
+    public class Show extends Applet {
+
+        //een (lege) methode die de Applet initialiseert
+        public void init() {
+            setBackground(Color.magenta);
+
+        }
+        //een methode die de inhoud van het scherm tekent
+        public void paint(Graphics g) {
+            g.setColor(Color.BLUE);
+            g.drawString("Welcome to Java!!", 50, 60 );
+        }
+
+    }
+
