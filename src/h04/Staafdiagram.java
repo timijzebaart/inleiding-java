d103e87cb3ec022eabf846b0dd1e43921da89be9
timijzebaart <tijzebaart@roc-dev.com>
2019-10-08 13:48:08 +0200

    Staafdiagram

---
+++ src/h04/Staafdiagram.java
@@ -0,0 +1,82 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Staafdiagram extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint (Graphics g) {
+
+        setBackground(Color.black);
+
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(90,60,200,200);
+        ///Achtergrond staafdiagram
+
+        g.setColor(Color.white);
+        g.drawRect(90,60,200,200);
+        ///witte rand
+
+        g.setColor(Color.white);
+        g.drawRect(130,80,160,160);
+        ///witte rand in de staafdiagram
+
+        g.setColor(Color.black);
+        g.drawString("Gewicht in KG", 100, 76);
+
+            g.setColor(Color.black);
+            g.drawLine(132, 240, 128, 240);
+            g.drawString("0", 120, 244);
+            ///0kg
+
+            g.drawLine(132, 220, 128, 220);
+            g.drawString("20", 113, 224);
+            ///20kg
+
+            g.drawLine(132, 200, 128, 200);
+            g.drawString("40", 113, 204);
+            ///40kg
+
+            g.drawLine(132, 180, 128, 180);
+            g.drawString("60", 113, 184);
+            ///60kg
+
+            g.drawLine(132, 160, 128, 160);
+            g.drawString("80", 113, 164);
+            ///80kg
+
+            g.drawLine(132, 140, 128, 140);
+            g.drawString("100", 105, 144);
+            ///100kg
+
+            g.drawLine(132, 120, 128, 120);
+            g.drawString("120", 105, 124);
+            ///120kg
+
+            g.drawLine(132, 100, 128, 100);
+            g.drawString("140", 105, 104);
+            ///140kg
+
+        g.setColor(Color.red);
+        g.fillRect(132, 140,40,100);
+        g.drawString("Jeroen", 130, 255);
+        ///100kg Jeroen
+
+        g.setColor(Color.blue);
+        g.fillRect(172, 160,40,80);
+        g.drawString("Hans", 175, 255);
+        ///80kg Hans
+
+        g.setColor(Color.yellow);
+        g.fillRect(212, 200,40,40);
+        g.drawString("Valerie", 215, 255);
+        ///40kg Valerie
+
+
+
+    }
+}
