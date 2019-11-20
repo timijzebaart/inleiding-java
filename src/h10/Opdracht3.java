1673773c894c1b01bb570ac2e0255e4a9371d013
timijzebaart <tijzebaart@roc-dev.com>
2019-11-20 09:25:46 +0100

    Opdracht 10.3

---
+++ src/h10/Opdracht3.java
@@ -0,0 +1,77 @@
+package h10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht3 extends Applet {
+    TextField tekstvak;
+    Label label;
+    String s, tekst;
+    int maand;
+
+    public void init() {
+        tekstvak = new TextField("", 20);
+        label = new Label("Type het maandnummer en druk op enter");
+        tekstvak.addActionListener( new TekstvakListener() );
+        tekst = "";
+        add(label);
+        add(tekstvak);
+    }
+
+    public void paint(Graphics g){
+        g.drawString(tekst, 50, 80);
+    }
+
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+            s = tekstvak.getText();
+            maand = Integer.parseInt( s);
+            switch(maand) {
+                case 1:
+                    tekst = "Januari, 31 dagen";
+                    break;
+                case 2:
+                    tekst = "Februari, 28 dagen";
+                    break;
+                case 3:
+                    tekst = "Maart, 31 dagen ";
+                    break;
+                case 4:
+                    tekst = "April, 30 dagen";
+                    break;
+                case 5:
+                    tekst = "Mei, 31 dagen";
+                    break;
+                case 6:
+                    tekst = "Juni, 30 dagen";
+                    break;
+                case 7:
+                    tekst = "Juli, 31 dagen";
+                    break;
+                case 8:
+                    tekst = "Augustus, 31 dagen";
+                    break;
+                case 9:
+                    tekst = "September, 30 dagen";
+                    break;
+                case 10:
+                    tekst = "Oktober, 31 dagen";
+                    break;
+                case 11:
+                    tekst = "November, 30 dagen";
+                    break;
+                case 12:
+                    tekst = "December, 31 dagen";
+                    break;
+                default:
+                    tekst = "Dit is helaas geen geldig maand nummer ..!";
+                    break;
+            }
+            repaint();
+        }
+    }
+}
+
+
