23f91e22778982434469487cca2aa7b2ce48d0bf
timijzebaart <tijzebaart@roc-dev.com>
2019-11-28 11:15:30 +0100

    Opdracht5 H12

---
+++ src/h12/Opdracht5.java
@@ -0,0 +1,57 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht5 extends Applet {
+
+    TextField tekstvak;
+    int[] getallen = {2, 4, 6, 8, 9, 12, 15, 17, 19};
+    Button knop;
+
+    public void init() {
+        tekstvak = new TextField("",20);
+        tekstvak.addActionListener( new KnopListener());
+
+        knop = new Button("OK");
+        knop.addActionListener( new KnopListener() );
+
+        add(tekstvak);
+        add(knop);
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    class KnopListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String inputString = tekstvak.getText();
+            int inputInt = Integer.parseInt(inputString);
+            int index = -1;
+
+            for (int i = 0; i < getallen.length; i++) {
+                if(getallen[i] == inputInt){
+                    index = i;
+                    break;
+                }
+            }
+            if (index != -1){
+                getGraphics().clearRect(0,0,400,400);
+                getGraphics().drawString("We hebben hem gevonden. Hij zit op index " + index, 50, 100);
+                tekstvak.setText("");
+
+            }
+            else {
+                getGraphics().clearRect(0,0,400,400);
+                getGraphics().drawString("We hebben hem helaas niet gevonden. ", 50, 100);
+                tekstvak.setText("");
+
+            }
+
+        }
+    }
+}
